88dfc0a9af446c6439de2c4137358922b03a174c
Brenton Collins <116970509+bjc2012@users.noreply.github.com>
2023-03-05 20:44:50 -0600

    Update bashdata.sh

---
+++ bashdata.sh
@@ -0,0 +1,9 @@
+# apache server install and start configuration
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo apt update -y
+  sudo apt install apache2 -y
+  echo "*** Completed Installing apache2"
+  EOF
